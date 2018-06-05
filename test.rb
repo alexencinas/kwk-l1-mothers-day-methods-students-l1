be9bc97d60ea0b9edaea2221966ff44afda49628
alexencinas <shoogiee123@gmail.com>
2018-06-05 18:35:35 +0000

    automatically backed up by learn

---
+++ test.rb
@@ -0,0 +1,22 @@
+#age = 9 
+# if age < 10
+# puts "You are too young. BYE!"
+#if age == 9
+# puts "welcome!"
+# end 
+#end
+
+#age 
+#if age > 18
+#puts "welcome"
+#else 
+#puts "Too young"
+#end 
+
+grade =50
+if grade >= 70
+  puts "You passed"
+else grade < 70
+    puts "You failed"
+end
+
